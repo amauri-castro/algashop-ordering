ec7fb4af552fd7485675e484c9134ade3b611deb
Amauri Castro <amauri.p.castro@gmail.com>
2025-11-06 22:19:23 -0300

    Implementado OrdersPersistenceProvider

---
+++ src/main/java/com/algashop/ordering/infrastructure/persistence/provider/OrdersPersistenceProvider.java
@@ -0,0 +1,43 @@
+package com.algashop.ordering.infrastructure.persistence.provider;
+
+import com.algashop.ordering.domain.model.entity.Order;
+import com.algashop.ordering.domain.model.repository.Orders;
+import com.algashop.ordering.domain.model.valueobject.id.OrderId;
+import com.algashop.ordering.infrastructure.persistence.entity.OrderPersistenceEntity;
+import com.algashop.ordering.infrastructure.persistence.repository.OrderPersistenceEntityRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class OrdersPersistenceProvider implements Orders {
+
+    private final OrderPersistenceEntityRepository persistenceRepository;
+
+    @Override
+    public Optional<Order> ofId(OrderId orderId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean exists(OrderId orderId) {
+        return false;
+    }
+
+    @Override
+    public void add(Order aggregateRoot) {
+        var persistenceEntity = OrderPersistenceEntity.builder()
+                .id(aggregateRoot.id().value().toLong())
+                .customerId(aggregateRoot.customerId().value())
+                .build();
+
+        persistenceRepository.saveAndFlush(persistenceEntity);
+    }
+
+    @Override
+    public int count() {
+        return 0;
+    }
+}
