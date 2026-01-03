f317de5e7e58908b7b2e6f91db5abd2c5c07e623
Amauri Castro <amauri.p.castro@gmail.com>
2026-01-02 22:22:51 -0300

    integração de bounded contexts e ACL

---
+++ src/main/java/com/algashop/ordering/domain/model/service/OriginAddressService.java
@@ -0,0 +1,8 @@
+package com.algashop.ordering.domain.model.service;
+
+import com.algashop.ordering.domain.model.valueobject.Address;
+
+public interface OriginAddressService {
+
+    Address originAddress();
+}
