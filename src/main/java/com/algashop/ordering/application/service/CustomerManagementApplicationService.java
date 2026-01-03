3895cad99f10744058c42647f3c0c8b202c9dbff
Amauri Castro <amauri.p.castro@gmail.com>
2026-01-03 16:28:20 -0300

    Implementado application service para Customer

---
+++ src/main/java/com/algashop/ordering/application/service/CustomerManagementApplicationService.java
@@ -0,0 +1,81 @@
+package com.algashop.ordering.application.service;
+
+import com.algashop.ordering.application.model.AddressData;
+import com.algashop.ordering.application.model.CustomerInput;
+import com.algashop.ordering.application.model.CustomerOutput;
+import com.algashop.ordering.domain.model.commons.*;
+import com.algashop.ordering.domain.model.customer.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Objects;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerManagementApplicationService {
+
+    private final CustomerRegistrationService customerRegistration;
+    private final Customers customers;
+
+    @Transactional
+    public UUID create(CustomerInput input) {
+        Objects.requireNonNull(input);
+        AddressData address = input.getAddress();
+
+        Customer customer = customerRegistration.register(
+                new FullName(input.getFirstName(), input.getLastName()),
+                new BirthDate(input.getBirthDate()),
+                new Email(input.getEmail()),
+                new Phone(input.getPhone()),
+                new Document(input.getDocument()),
+                input.getPromotionNotificationsAllowed(),
+                Address.builder()
+                        .street(address.getStreet())
+                        .number(address.getNumber())
+                        .complement(address.getComplement())
+                        .neighborhood(address.getNeighborhood())
+                        .city(address.getCity())
+                        .state(address.getState())
+                        .zipCode(new ZipCode(address.getZipCode()))
+                        .build()
+        );
+
+        customers.add(customer);
+
+        return customer.id().value();
+    }
+
+    @Transactional
+    public CustomerOutput findById(UUID customerId) {
+        Objects.requireNonNull(customerId);
+        Customer customer = customers.ofId(new CustomerId(customerId))
+                .orElseThrow(() -> new CustomerNotFoundException());
+
+        return CustomerOutput.builder()
+                .id(customer.id().value())
+                .firstName(customer.fullName().firstName())
+                .lastName(customer.fullName().lastName())
+                .email(customer.email().value())
+                .document(customer.document().value())
+                .phone(customer.phone().value())
+                .promotionNotificationsAllowed(customer.isPromotionNotificationsAllowed())
+                .loyaltyPoints(customer.loyaltyPoints().value())
+                .registeredAt(customer.registeredAt())
+                .archived(customer.isArchived())
+                .archivedAt(customer.archivedAt() != null ? customer.archivedAt() : null)
+                .birthDate(customer.birthDate() != null ? customer.birthDate().value() : null)
+                .address(AddressData.builder()
+                        .street(customer.address().street())
+                        .number(customer.address().number())
+                        .complement(customer.address().complement())
+                        .neighborhood(customer.address().neighborhood())
+                        .city(customer.address().city())
+                        .state(customer.address().state())
+                        .zipCode(customer.address().zipCode().value())
+                        .build())
+
+                .build();
+    }
+}
