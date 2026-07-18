885ce9672717110b5dbedd2ebc37ef2914b660ac
Amauri Castro <amauri.p.castro@gmail.com>
2026-07-18 12:06:30 -0300

    aprimorado auditoria com SecurityCheckApplicationService

--- src/main/java/com/algashop/ordering/infrastructure/config/auditing/SpringDataAuditingConfig.java
+++ src/main/java/com/algashop/ordering/infrastructure/config/auditing/SpringDataAuditingConfig.java
@@ -1,5 +1,6 @@
 package com.algashop.ordering.infrastructure.config.auditing;
 
+import com.algashop.ordering.core.application.security.SecurityCheckApplicationService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.auditing.DateTimeProvider;
@@ -26,7 +27,13 @@ public class SpringDataAuditingConfig {
     }
 
     @Bean
-    public AuditorAware<UUID> auditorProvider() {
-        return () -> Optional.of(UUID.randomUUID());
+    public AuditorAware<UUID> auditorProvider(SecurityCheckApplicationService securityCheck) {
+
+        return () -> {
+            if (!securityCheck.isAuthenticated() || securityCheck.isMachineAuthenticated()) {
+                return Optional.empty();
+            }
+            return Optional.of(securityCheck.getAuthenticatedUserId());
+        };
     }
 }
